5ef9add4bad68f4591a3355387471f692f470382
Cyril PHAM-LE <cyrilphamle@gmail.com>
2017-09-17 14:27:25 +0200

    Next state of O X O is O O O

--- src/main/java/fr/gameoflife/domain/GameOfLife.kt
+++ src/main/java/fr/gameoflife/domain/GameOfLife.kt
@@ -1,5 +1,41 @@
 package fr.gameoflife.domain
 
-class GameOfLife(initialGrid: String) {
-  fun nextState(): String = "O"
+class GameOfLife(val initialGrid: String) {
+  var grid: List<GCell> = toGrid()
+
+  fun nextState(): String {
+    updateState()
+
+    return grid.map { it.state }.joinToString(" ")
+  }
+
+  private fun updateState() {
+    val updatedGrid: MutableList<GCell> = mutableListOf()
+
+    grid.forEach { cell ->
+      val neighbors: List<GCell> = findNeighbors(cell)
+      val updatedCell: GCell = applyRules(cell, neighbors)
+      updatedGrid.add(updatedCell)
+    }
+
+    grid = updatedGrid
+  }
+
+  private fun applyRules(cell: GCell, neighbors: List<GCell>): GCell {
+    val numberOfAlive: Int = neighbors.count { it.state == "X" }
+    if (numberOfAlive < 2)
+      return GCell(cell.columnIndex, "O")
+    else
+      return GCell(cell.columnIndex, cell.state)
+  }
+
+  private fun findNeighbors(cell: GCell): List<GCell> {
+    return grid.filter { it.columnIndex == cell.columnIndex - 1 || it.columnIndex == cell.columnIndex + 1 }
+  }
+
+  private fun toGrid(): List<GCell> {
+    return this.initialGrid.split(" ").mapIndexed { columnIndex, state -> GCell(columnIndex, state) };
+  }
 }
+
+data class GCell(val columnIndex: Int, val state: String)
--- src/test/java/fr/gameoflife/domain/GameOfLifeTest.kt
+++ src/test/java/fr/gameoflife/domain/GameOfLifeTest.kt
@@ -6,8 +6,13 @@ import org.junit.Test
 class GameOfLifeTest {
   @Test
   fun `should have the state of game when there is one living cell after one iteration`() {
-    val game = GameOfLife("X");
-    assertThat(game.nextState()).isEqualTo("O");
+    val game = GameOfLife("X")
+    assertThat(game.nextState()).isEqualTo("O")
+  }
+
+  @Test
+  fun `should have a game of 3 dead cells when there is less than 2 living neighbor`() {
+    val game = GameOfLife("O X O")
+    assertThat(game.nextState()).isEqualTo("O O O")
   }
 }
-
